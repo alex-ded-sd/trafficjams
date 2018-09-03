f1f08b70d4641a027090cc1a270c5283e446c3f2
Aleksandr <a.didenko.sd@gmail.com>
2018-09-03 10:55:48 +0300

    create infrastructure for web client

---
+++ TrafficJams.WebClient/Controller/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace TrafficJams.WebClient.Controller
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Controller = Microsoft.AspNetCore.Mvc.Controller;
+
+	public class HomeController : Controller
+	{
+		// GET
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
